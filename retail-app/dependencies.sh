7ab5cdf2a3e5f98b9ec157fc6f6ef1cc927f9c4c
devarias <gedafu2005@gmail.com>
2021-04-23 09:14:27 -0500

    Deleted package.json, package-lock.json, and added the dependencies to install and start the web app in localhost

--- retail-app/dependencies.sh
+++ retail-app/dependencies.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
-npm i -S @auth0/auth0-react
-npm i -S react-router-dom
-npm i -S antd
-npm i -S axios
-npm i -S react-highlight-words
-npm i -S react-scripts
-npm i -S react-dom
+function install_packages {
+    npm init -y
+    npm i -S react
+    npm i -S @auth0/auth0-react
+    npm i -S react-router-dom
+    npm i -S antd
+    npm i -S axios
+    npm i -S react-highlight-words
+    npm i -S react-scripts
+    npm i -S react-dom
+    npm i -S web-vitals
+    sed -i '7i \\t\t"start": "react-scripts start",' package.json
+    echo "Y" | npm start
+}
+
+if [ -d "./node_modules/" ]
+then
+    rm -rf node_modules package.json package-lock.json
+    install_packages
+else
+    install_packages
+fi
