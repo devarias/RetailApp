0af78c06a0685460068485c6eca5301613d6575e
devarias <gedafu2005@gmail.com>
2021-04-22 21:12:48 -0500

    Added the setup for the MySQL Database

--- retail-api/sql/retailDatabase.sql
+++ retail-api/sql/retailDatabase.sql
@@ -1,6 +1,3 @@
-
--- CREATION OF THE DATABASE AND USER
-CREATE DATABASE IF NOT EXISTS retail_app;
 USE retail_app;
 
 -- TABLES CREATION
@@ -16,14 +13,14 @@ CREATE TABLE IF NOT EXISTS users(
 );
 
 CREATE TABLE countries(
-    id_ INT AUTO_INCREMENT NOT NULL,
+    id_ INT,
     country_name VARCHAR(50) NOT NULL,
     shipping_cost FLOAT(2,2) NOT NULL,
     PRIMARY KEY (id_)
 );
 
 CREATE TABLE states(
-    id_ INT AUTO_INCREMENT,
+    id_ INT,
     state_name VARCHAR(50) NOT NULL,
     country_id INT NOT NULL,
     FOREIGN KEY (country_id) REFERENCES countries(id_),
@@ -31,7 +28,7 @@ CREATE TABLE states(
 );
 
 CREATE TABLE cities(
-    id_ INT AUTO_INCREMENT NOT NULL,
+    id_ INT,
     city_name VARCHAR(50) NOT NULL,
     state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id_),
@@ -77,43 +74,43 @@ CREATE TABLE IF NOT EXISTS payments(
     FOREIGN KEY (order_id) REFERENCES orders(id_)
 );
 
-INSERT INTO countries(country_name, shipping_cost) VALUES
-    ('Colombia', 0.05),
-    ('Estados Unidos', 0.20),
-    ('Republica Dominicana', 0.05),
-    ('Brasil', 0.15),
-    ('Canada', 0.20);
+INSERT INTO countries(id_, country_name, shipping_cost) VALUES
+    (1, 'Colombia', 0.05),
+    (2, 'Estados Unidos', 0.20),
+    (3, 'Republica Dominicana', 0.05),
+    (4, 'Brasil', 0.15),
+    (5, 'Canada', 0.20);
 
-INSERT INTO states(state_name, country_id) VALUES
-    ('Antioquia', 1),
-    ('Norte de Santander', 1),
-    ('Florida', 2),
-    ('Pennsylvania', 2),
-    ('Distrito Nacional', 3),
-    ('Santiago', 3),
-    ('Rio de Janeiro', 4),
-    ('Parana', 4),
-    ('Ontario', 5),
-    ('Quebec', 5);
+INSERT INTO states(id_, state_name, country_id) VALUES
+    (1, 'Antioquia', 1),
+    (2, 'Norte de Santander', 1),
+    (3, 'Florida', 2),
+    (4, 'Pennsylvania', 2),
+    (5, 'Distrito Nacional', 3),
+    (6, 'Santiago', 3),
+    (7, 'Rio de Janeiro', 4),
+    (8, 'Parana', 4),
+    (9, 'Ontario', 5),
+    (10, 'Quebec', 5);
 
-INSERT INTO cities(city_name, state_id) VALUES
-    ('Medellin', 1),
-    ('Sabaneta', 1),
-    ('Cucuta', 2),
-    ('Pamplona', 2),
-    ('Orlando', 3),
-    ('Miami', 3),
-    ('Philadelphia', 4),
-    ('Pittsburgh', 4),
-    ('Santo Domingo', 5),
-    ('Miraflores', 5),
-    ('Santiago', 6),
-    ('Tamboril', 6),
-    ('Petropolis', 7),
-    ('Rio de Janeiro', 7),
-    ('Paranagua', 8),
-    ('Tibagi', 8),
-    ('Toronto', 9),
-    ('Ottawa', 9),
-    ('Quebec', 10),
-    ('Montreal', 10);
+INSERT INTO cities(id_, city_name, state_id) VALUES
+    (1, 'Medellin', 1),
+    (2, 'Sabaneta', 1),
+    (3, 'Cucuta', 2),
+    (4, 'Pamplona', 2),
+    (5, 'Orlando', 3),
+    (6, 'Miami', 3),
+    (7, 'Philadelphia', 4),
+    (8, 'Pittsburgh', 4),
+    (9, 'Santo Domingo', 5),
+    (10, 'Miraflores', 5),
+    (11, 'Santiago', 6),
+    (12, 'Tamboril', 6),
+    (13, 'Petropolis', 7),
+    (14, 'Rio de Janeiro', 7),
+    (15, 'Paranagua', 8),
+    (16, 'Tibagi', 8),
+    (17, 'Toronto', 9),
+    (18, 'Ottawa', 9),
+    (19, 'Quebec', 10),
+    (20, 'Montreal', 10);
---
+++ retail-api/sql/setupMySQL.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS retail_app;
+CREATE USER IF NOT EXISTS 'retail_user'@'localhost' IDENTIFIED BY 'Password123#@!';
+GRANT ALL PRIVILEGES ON `retail_app`.* TO 'retail_user'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'retail_user'@'localhost';
+FLUSH PRIVILEGES;
