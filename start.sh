e5af176d33486e037be887b04a6232566de344b9
devarias <gedafu2005@gmail.com>
2021-04-23 13:01:12 -0500

    Fixed some shellcheck requirements

--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 start () {
-    cd retail-app
+    cd retail-app || exit
     DATABASE_URL=mysql://retail_user:Password123#@\!@localhost/retail_app ../retail-api/./main.py &>/dev/null & ./dependencies.sh && fg
 }
 printf "If this is your first time.
 Install the app and it will run automatically,
-otherwise you can start the app.\n\nSelect the option to start
-\n1. Install the app\n2. Start the app\t"
-read option
-if [ $option -eq 1 ]
+otherwise you can start the app.
+
+Select the option to start
+1. Install the app
+2. Start the app    "
+read -r option
+if [[ $option -eq 1 ]]
 then
-    cat retail-api/sql/setupMySQL.sql | sudo mysql -uroot -hlocalhost -p
-    cat retail-api/sql/retailDatabase.sql | sudo mysql -uretail_user -hlocalhost -pPassword123#@! &>/dev/null
+    sudo mysql -uroot -hlocalhost -p < retail-api/sql/setupMySQL.sql
+    sudo mysql -uretail_user -hlocalhost -pPassword123#@! &>/dev/null < retail-api/sql/retailDatabase.sql
     start
 else
     start
