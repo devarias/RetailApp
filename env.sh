90d8dd7f3ea2915c6b4529d2829582187e2b0037
devarias <gedafu2005@gmail.com>
2021-04-24 09:04:56 -0500

    Added the waiting message

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo printf "Wait we are setting the virtual enviroment for you
+this could take some seconds...\\n"
 sudo pip3 install virtualenv &>/dev/null
 virtualenv -p python3 venv &>/dev/null
 . venv/bin/activate
